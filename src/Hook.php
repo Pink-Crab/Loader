c04f7d3a18d559525d8dc75d12fda0a433ca8baf
gin0115 <glynn.quelch@gmail.com>
2021-03-20 16:01:58 +0000

    Base model in place

--- src/Hook.php
+++ src/Hook.php
@@ -27,6 +27,8 @@ namespace PinkCrab\Loader;
 
 class Hook {
 
+	/**  Hook type constants. */
+
 	/** @var string */
 	public const ACTION = 'action';
 	/** @var string */
@@ -38,27 +40,71 @@ class Hook {
 	/** @var string */
 	public const REMOVE = 'remove';
 
-	/** @var string */
+
+	/**
+	 * The hooks type
+	 * @var string
+	 * */
 	protected $type = self::ACTION;
-	/** @var string */
+
+	/**
+	 * The hooks handle
+	 * @var string
+	 * */
 	protected $handle;
-	/** @var callable */
+
+	/**
+	 * The hooks callback
+	 * @var callable
+	 * */
 	protected $callback;
-	/** @var int */
+
+	/**
+	 * Hook priority (defualts to 10 as per WP Core)
+	 * @var int
+	 * */
 	protected $priority = 10;
-	/** @var int */
+
+	/**
+	 * Callback arg count (defualts to 1 as per WP Core)
+	 * @var int
+	 * */
 	protected $args = 1;
-	/** @var bool */
+
+	/**
+	 * If ajax should be registered with priv
+	 * @var bool
+	 * */
 	protected $ajax_private = true;
-	/** @var bool */
+
+	/**
+	 * If ajax should be registerd with nopriv
+	 * @var bool
+	 * */
 	protected $ajax_public = true;
-	/** @var bool */
+
+	/**
+	 * Lazy evaluation off callback
+	 * @var bool
+	 * */
 	protected $lazy = false;
-	/** @var string|null */
+
+	/**
+	 * If this hook should has its creation deffered to another hook
+	 * @var string|null
+	 * */
 	protected $deffered_on = null;
-	/** @var bool */
+
+	/**
+	 * Should this hook be loaded if is_admin === true
+	 * @var bool
+	 * */
 	protected $is_admin = true;
-	/** @var bool */
+
+	/**
+	 * Should this hook be loaded if is_admin === false
+	 * @var bool
+	 * */
 	protected $is_front = true;
 
 	public function __construct(
@@ -71,6 +117,222 @@ class Hook {
 		$this->callback = $callback;
 		$this->priority = $priority;
 		$this->args     = $args;
+	}
+
+	/**
+	 * Get the value of type
+	 */
+	public function get_type(): string {
+		return $this->type;
+	}
+
+	/**
+	 * Set the value of type
+	 *
+	 * @param string $type
+	 * @return self
+	 */
+	public function type( string $type ): self {
+		$this->type = $type;
+		return $this;
+	}
+
+	/**
+	 * Get the hooks handle
+	 * @return string
+	 */
+	public function get_handle(): string {
+		return $this->handle;
+	}
+
+	/**
+	 * Set the hooks handle
+	 *
+	 * @param string $handle  The hooks handle
+	 * @return self
+	 */
+	public function handle( string $handle ): self {
+		$this->handle = $handle;
+		return $this;
+	}
+
+	/**
+	 * Get the hooks callback
+	 * @return callable
+	 */
+	public function get_callback(): callable {
+		return $this->callback;
+	}
+
+	/**
+	 * Set the hooks callback
+	 *
+	 * @param callable $callback  The hooks callback
+	 * @return self
+	 */
+	public function callback( callable $callback ): self {
+		$this->callback = $callback;
+		return $this;
+	}
+
+	/**
+	 * Get hook priority (defualts to 10 as per WP Core)
+	 * @return int
+	 */
+	public function get_priority(): int {
+		return $this->priority;
+	}
+
+	/**
+	 * Set hook priority (defualts to 10 as per WP Core)
+	 *
+	 * @param int $priority  Hook priority (defualts to 10 as per WP Core)
+	 * @return self
+	 */
+	public function priority( int $priority ): self {
+		$this->priority = $priority;
+		return $this;
+	}
+
+	/**
+	 * Get callback arg count (defualts to 1 as per WP Core)
+	 * @return int
+	 */
+	public function args_count(): int {
+		return $this->args;
+	}
+
+	/**
+	 * Set callback arg count (defualts to 1 as per WP Core)
+	 *
+	 * @param int $args  Callback arg count (defualts to 1 as per WP Core)
+	 * @return self
+	 */
+	public function args( int $args ): self {
+		$this->args = $args;
+		return $this;
+	}
+
+	/**
+	 * Get if ajax should be registered with priv
+	 * @return bool
+	 */
+	public function is_ajax_private(): bool {
+		return $this->ajax_private;
+	}
+
+	/**
+	 * Set if ajax should be registered with priv
+	 *
+	 * @param bool $ajax_private  If ajax should be registered with priv
+	 * @return self
+	 */
+	public function ajax_private( bool $ajax_private = true ): self {
+		$this->ajax_private = $ajax_private;
+		return $this;
+	}
+
+	/**
+	 * Get if ajax should be registerd with nopriv
+	 * @return bool
+	 */
+	public function is_ajax_public(): bool {
+		return $this->ajax_public;
+	}
+
+	/**
+	 * Set if ajax should be registerd with nopriv
+	 *
+	 * @param bool $ajax_public  If ajax should be registerd with nopriv
+	 * @return self
+	 */
+	public function ajax_public( bool $ajax_public = true ): self {
+		$this->ajax_public = $ajax_public;
+		return $this;
+	}
+
+	/**
+	 * Get lazy evaluation off callback
+	 * @return bool
+	 */
+	public function is_lazy(): bool {
+		return $this->lazy;
+	}
+
+	/**
+	 * Set lazy evaluation off callback
+	 *
+	 * @param bool $lazy  Lazy evaluation off callback
+	 * @return self
+	 */
+	public function lazy( bool $lazy = true ): self {
+		$this->lazy = $lazy;
+		return $this;
+	}
+
+	/**
+	 * Get if this hook should has its creation deffered to another hook
+	 * @return string|null
+	 */
+	public function get_deffered_on(): ?string {
+		return $this->deffered_on;
+	}
+
+	/**
+	 * Checks if the hook is to be deffered
+	 * @return bool
+	 */
+	public function is_deffered(): bool {
+		return $this->deffered_on !== null;
+	}
+
+	/**
+	 * Set if this hook should has its creation deffered to another hook
+	 *
+	 * @param string|null $deffered_on  If this hook should has its creation deffered to another hook
+	 * @return self
+	 */
+	public function deffered_on( $deffered_on ): self {
+		$this->deffered_on = $deffered_on;
+		return $this;
+	}
+
+	/**
+	 * Get should this hook be loaded if is_admin === true
+	 * @return bool
+	 */
+	public function is_admin(): bool {
+		return $this->is_admin;
+	}
+
+	/**
+	 * Set should this hook be loaded if is_admin === true
+	 *
+	 * @param bool $is_admin  Should this hook be loaded if is_admin === true
+	 * @return self
+	 */
+	public function admin( bool $is_admin = true ): self {
+		$this->is_admin = $is_admin;
+		return $this;
+	}
+
+	/**
+	 * Get should this hook be loaded if is_admin === false
+	 * @return bool
+	 */
+	public function is_front(): bool {
+		return $this->is_front;
+	}
+
+	/**
+	 * Set should this hook be loaded if is_admin === false
+	 *
+	 * @param bool $is_front  Should this hook be loaded if is_admin === false
+	 * @return self
+	 */
+	public function front( bool $is_front = true ): self {
+		$this->is_front = $is_front;
+		return $this;
 	}
 }
 
